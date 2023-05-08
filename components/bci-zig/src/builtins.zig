8d14f7e6734cb4aaae84a54880e6d0ec12e57ec3
Graeme Lockley <graeme.lockley@gmail.com>
2023-05-08 14:26:18 +0200

    feature: add string concat and string equal

--- components/bci-zig/src/builtins.zig
+++ components/bci-zig/src/builtins.zig
@@ -43,6 +43,40 @@ fn string_length(state: *Machine.MemoryState) error{OutOfMemory}!void {
     _ = try state.push_int_value(@intCast(i32, v.v.s.len));
 }
 
+fn string_concat(state: *Machine.MemoryState) error{OutOfMemory}!void {
+    _ = try state.push_builtin_closure_value(state.peek(1), state.peek(0), string_concat_2);
+    state.stack.items[state.stack.items.len - 3] = state.stack.items[state.stack.items.len - 1];
+    _ = state.pop();
+    _ = state.pop();
+}
+
+fn string_concat_2(state: *Machine.MemoryState) error{OutOfMemory}!void {
+    const v1 = state.pop();
+    const v2 = state.pop();
+
+    var buffer = std.ArrayList(u8).init(state.allocator);
+    defer buffer.deinit();
+
+    try buffer.appendSlice(v2.v.bc.argument.v.s);
+    try buffer.appendSlice(v1.v.s);
+
+    _ = try state.push_string_value(buffer.items);
+}
+
+fn string_equal(state: *Machine.MemoryState) error{OutOfMemory}!void {
+    _ = try state.push_builtin_closure_value(state.peek(1), state.peek(0), string_equal_2);
+    state.stack.items[state.stack.items.len - 3] = state.stack.items[state.stack.items.len - 1];
+    _ = state.pop();
+    _ = state.pop();
+}
+
+fn string_equal_2(state: *Machine.MemoryState) error{OutOfMemory}!void {
+    const v1 = state.pop();
+    const v2 = state.pop();
+
+    _ = try state.push_bool_value(std.mem.eql(u8, v1.v.s, v2.v.bc.argument.v.s));
+}
+
 fn string_substring(state: *Machine.MemoryState) error{OutOfMemory}!void {
     _ = try state.push_builtin_closure_value(state.peek(1), state.peek(0), string_substring_2);
     state.stack.items[state.stack.items.len - 3] = state.stack.items[state.stack.items.len - 1];
@@ -76,6 +110,8 @@ fn string_substring_3(state: *Machine.MemoryState) error{OutOfMemory}!void {
         _ = try state.push_string_value("");
     } else if (arg3 <= arg2) {
         _ = try state.push_string_value("");
+    } else if (arg3 >= arg1.len) {
+        _ = try state.push_string_value(arg1[@intCast(usize, arg2)..]);
     } else {
         _ = try state.push_string_value(arg1[@intCast(usize, arg2)..@intCast(usize, arg3)]);
     }
@@ -88,6 +124,10 @@ pub fn find(name: []const u8) ?(*const fn (state: *Machine.MemoryState) error{Ou
         return print;
     } else if (std.mem.eql(u8, name, "$$builtin-print-literal")) {
         return print_literal;
+    } else if (std.mem.eql(u8, name, "$$builtin-string-concat")) {
+        return string_concat;
+    } else if (std.mem.eql(u8, name, "$$builtin-string-equal")) {
+        return string_equal;
     } else if (std.mem.eql(u8, name, "$$builtin-string-length")) {
         return string_length;
     } else if (std.mem.eql(u8, name, "$$builtin-string-substring")) {
